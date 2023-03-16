d6111cfabfb754c7ff00b5ff0dec6e0e1febb667
JunioRDev <carmelojunior020417@gmail.com>
2023-03-16 18:44:25 -0400

    Implementa cálculo de salario basado en días trabajados y pago por día
    - Añade función para solicitar nombre, pago por día y días trabajados del usuario
    
    - Crea método 'CalcularSalario' para calcular el salario total
    
    - Muestra el salario calculado en formato de moneda y la cantidad de días trabajados

---
+++ Solución_CSharp_NivelBásico/MetodosConParametroYretornaResultados/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Salario
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //TAREA
+        //SOLICITAR NOMBRE, CUANTO ME PAGAN POR DIA TRABAJADO Y LOS DIAS TRABAJADOS
+        //Y EN BASE A ESO MI PROGRAMACION DEBE DECIRME CUANTO HE GANADO O DEBO COBRAR
+        {
+            string nombre;
+            decimal pagoPorDia, salario;
+            int diasTrabajados;
+            // Solicitar nombre
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            // Solicitar cuánto le pagan por día trabajado
+            Console.Write("Ingrese cuánto le pagan por día trabajado: ");
+            pagoPorDia = decimal.Parse(Console.ReadLine());
+
+            // Solicitar los días trabajados
+            Console.Write("Ingrese los días trabajados: ");
+            diasTrabajados = int.Parse(Console.ReadLine());
+
+            // Calcular el salario
+            salario = CalcularSalario(pagoPorDia, diasTrabajados);
+
+            // Mostrar el resultado
+            Console.WriteLine($"\n{nombre}, usted debe cobrar {salario:C} por {diasTrabajados} días trabajados.");
+            Console.ReadKey();
+        }
+        //Método para obtener el total del salario osea del dinero ganado por trabajo
+        static decimal CalcularSalario(decimal pagoPorDia, int diasTrabajados)
+        {
+            return pagoPorDia * diasTrabajados;
+        }
+    }
+}
